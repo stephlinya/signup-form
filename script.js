3f85c77ee83c8d0c154245d47787d792c16e9fb2
Stephanie Lin <stephlinya@gmail.com>
2021-09-13 18:09:55 -0700

    added errors for empty fields and some css styling on active error state

--- script.js
+++ script.js
@@ -3,20 +3,38 @@ let lastName = document.getElementById('last-name');
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 let form = document.getElementById('form');
-let errorElement = document.getElementById('error');
+let firstNameError = document.getElementById('first-name__error');
+let lastNameError = document.getElementById('last-name__error');
+let emailError = document.getElementById('email__error');
+let passwordError = document.getElementById('password__error');
 
 form.addEventListener('submit', (e) => {
-    let messages = [];
+    let firstNameMessages = [];
+    let lastNameMessages = [];
+    let emailMessages = [];
+    let passwordMessages = [];
+
     if (firstName.value === '' || firstName.value == null) {
-        messages.push('First Name is required');
+        firstNameMessages.push('First Name cannot be empty');
     }
 
     if (lastName.value === '' || lastName.value == null) {
-        messages.push('Last Name is required');
+        lastNameMessages.push('Last Name cannot be empty');
     }
 
-    if (messages.length > 0) {
+    if (email.value === '' || email.value == null) {
+        emailMessages.push('Email cannot be empty');
+    }
+
+    if (password.value === '' || password.value == null) {
+        passwordMessages.push('Password cannot be empty');
+    }
+
+    if (firstNameMessages.length > 0 || lastNameMessages.length > 0 || emailMessages.length > 0 || passwordMessages.length > 0) {
         e.preventDefault(); 
-        errorElement.innerText = messages.join (", ");
+        firstNameError.innerText = firstNameMessages;
+        lastNameError.innerText = lastNameMessages;
+        emailError.innerText = emailMessages;
+        passwordError.innerText = passwordMessages;
     }
 })
