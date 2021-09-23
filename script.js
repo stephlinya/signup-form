53085ed41bd0016113dfbe67e36acf57553dbaad
Stephanie Lin <stephlinya@gmail.com>
2021-09-23 16:49:12 -0700

    added isValid function for form validation

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let firstNameError = document.getElementById('first-name__error');
 let lastNameError = document.getElementById('last-name__error');
 let emailError = document.getElementById('email__error');
 let passwordError = document.getElementById('password__error');
+let isValid = true;
 
 function validateEmail(email){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
@@ -18,25 +19,31 @@ form.addEventListener('submit', (e) => {
     let lastNameMessages = [];
     let emailMessages = [];
     let passwordMessages = [];
+    isValid;
 
     if (firstName.value === '' || firstName.value == null) {
         firstNameMessages.push('First Name cannot be empty');
+        isValid = false;
     }
 
     if (lastName.value === '' || lastName.value == null) {
         lastNameMessages.push('Last Name cannot be empty');
+        isValid = false;
     }
 
     if (email.value === '' || email.value == null) {
         emailMessages.push('Email cannot be empty'); 
+        isValid = false;
     } else if(!validateEmail(email)){
             emailMessages.push('Email is not valid');
+            isValid = false;
     } else {
         emailMessages.push('Email is valid');
     }
 
     if (password.value === '' || password.value == null) {
         passwordMessages.push('Password cannot be empty');
+        isValid = false;
     }
 
     if (firstNameMessages.length > 0 || lastNameMessages.length > 0 || emailMessages.length > 0 || passwordMessages.length > 0) {
@@ -46,4 +53,11 @@ form.addEventListener('submit', (e) => {
         emailError.innerText = emailMessages;
         passwordError.innerText = passwordMessages;
     }
+    if (isValid === false) {
+        document.getElementById("first-name").className = document.getElementById("first-name").className + "error";
+    } else {
+        document.getElementById("first-name").className = document.getElementById("first-name").className.replace("error", "focus");
+    }
+
 })
+
